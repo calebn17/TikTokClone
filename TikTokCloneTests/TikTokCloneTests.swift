cedce1b20e2db7b0d1f35ef30f5ebddd3624e9eb
calebn17 <ccngai17@utexas.edu>
2022-06-22 12:29:59 -0500

    add some unit tests

--- TikTokCloneTests/TikTokCloneTests.swift
+++ TikTokCloneTests/TikTokCloneTests.swift
@@ -7,30 +7,18 @@
 
 import XCTest
 @testable import TikTokClone
+import StoreKit
 
 class TikTokCloneTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testPostChildPath() {
+        let id = UUID().uuidString
+        let user = User(username: "billgates", profilePictureURL: nil, identifier: "123")
+        var post = PostModel(identifier: id, user: user)
+        XCTAssertTrue(post.caption.isEmpty)
+        post.caption = "Hello!"
+        XCTAssertFalse(post.caption.isEmpty)
+        XCTAssertEqual(post.videoChildPath, "videos/billgates/")
     }
 
 }
